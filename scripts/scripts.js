da04562e387270c458390b4ba4d5eb5e5d96bb70
DoanTheDev <121005469+DoanTheDev@users.noreply.github.com>
2023-08-01 01:43:29 -0400

    testing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,18 @@
-const arr1 = ['Rock','Paper','Scissors'];
+const arr1 = ['rock','paper','scissors'];
 
-const computerChoices = arr1[Math.floor(Math.random() * arr1.length)];
+function getComputerChoice() {return arr1[Math.floor(Math.random() * arr1.length)]};
 
-console.log(computerChoices);
+
+const computerSelection = getComputerChoice();
+
+function getPlayerChoice(){return prompt('Rock, Paper, or Scissors?') };
+
+const playerSelection = getPlayerChoice();
+
+function playRound(playerSelection,computerSelection) { 
+    if (playerSelection === 'rock', computerSelection ===[2] || playerSelection === 'scissors', computerSelection ===[1] || playerSelection === 'paper', computerSelection ===[0] ) { alert('Win!')}
+    else if( playerSelection === 'rock', computerSelection ===[1] || playerSelection ==='scissors', computerSelection===[0] || playerSelection === 'paper', computerSelection===[2] ) {return alert('Lose!')}
+    else{playerSelection === computerSelection} {return alert('DRAW!')}
+};
+        
+ console.log(playRound(playerSelection,computerSelection));
