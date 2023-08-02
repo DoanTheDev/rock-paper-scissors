7d86a52884742f5d296be47a41d9551eb364d3e4
DoanTheDev <121005469+DoanTheDev@users.noreply.github.com>
2023-08-02 14:25:41 -0400

    made some conditions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,22 @@
 function getComputerChoice() {
-    const choice = ['rock','paper','scissors'];
-   return choice[Math.floor(Math.random() * choice.length)];
-};
+    const computerChoice = ['rock','paper','scissors'];
+   return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+}
+function getPlayerChoice() {
+    let playerChoice = prompt('Rock, Paper , or Scissors?') 
+     
+}
+ 
 
-const playerSelection = "rock";
+function playRound(playerSelection,computerSelection) { if (playerSelection == 'rock', computerSelection === [2] || playerSelection == 'paper', computerSelection === [0] || playerSelection == 'scissors', computerSelection === [1]) {alert('W!')}
+ else if(playerSelection == 'rock', computerSelection === [1] || playerSelection == 'paper', computerSelection === [2] || playerSelection == 'scissors', computerSelection === [0]) {alert('L!')}
+  else(playerSelection === computerSelection) {alert('D!')};
+
+}
+
+
+
+const playerSelection = getPlayerChoice()
 const computerSelection = getComputerChoice()
-console.log(computerSelection)
+
+console.log(playRound(playerSelection,computerSelection))
